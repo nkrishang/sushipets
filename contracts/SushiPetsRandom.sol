804d0ba41e2f9b6a4c59b3899174330df3a6b827
nkrishang <kn1457@nyu.edu>
2021-05-15 12:57:22 -0400

    Added SushiPetsRandom

---
+++ contracts/SushiPetsRandom.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract SushiPetsRandom {
+
+    constructor() {}
+
+    function getRandomNumber(uint _range) public {
+        // Not impemented yet.
+    }
+}
