bc84a27f82e2fda02522fa12faaed3ff5c112df9
nkrishang <kn1457@nyu.edu>
2021-05-15 13:03:58 -0400

    Updated SushiPetsMarket

--- contracts/SushiPetsMarket.sol
+++ contracts/SushiPetsMarket.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import './SushiPetsStore.sol';
+import './SushiPetsToken.sol';
 
 contract SushiPetsMarket {
 
-    SushiPetsStore internal sushiPetsStore;
+    SushiPetsToken internal sushiPetsToken;
 
     struct Bid {
         address bidder;
@@ -24,13 +24,13 @@ contract SushiPetsMarket {
     event NewBid(uint indexed tokenID, address indexed bidder, uint bidValue);
     event BidAccepted(uint indexed tokenID, address indexed owner, address indexed bidder, uint bidValue);
 
-    constructor(address _petStore) {
-
+    constructor(address _tokenAddress) {
+        sushiPetsToken = SushiPetsToken(_tokenAddress);
     }
 
     function setThreshold(uint _tokenId, uint _value, bool _active) external {
-        require(_exists(_tokenId), "ERC721: token has not been minted.");
-        require(msg.sender == ownerOf(_tokenId), "Only the token owner can accept the bid.");
+        require(sushiPetsToken.totalPetsMinted() <= _tokenId, "ERC721: token has not been minted.");
+        require(msg.sender == sushiPetsToken.ownerOf(_tokenId), "Only the token owner can accept the bid.");
 
         threshold[_tokenId].value = _value;
         threshold[_tokenId].active = _active;
@@ -41,7 +41,7 @@ contract SushiPetsMarket {
         
         Bid memory bid = bids[_tokenId];
 
-        require(_exists(_tokenId), "ERC721: token has not been minted.");
+        require(sushiPetsToken.totalPetsMinted() <= _tokenId, "ERC721: token has not been minted.");
         require(_bidValue == msg.value, "Must lock up the bid amount in the contract.");
         require(_bidValue > bid.bidValue, "The new bid value must exceed the current bid value.");
 
@@ -52,7 +52,7 @@ contract SushiPetsMarket {
 
         if(threshold[_tokenId].active && msg.value >= threshold[_tokenId].value) {
             
-            (bool sent,) = ownerOf(_tokenId).call{value: msg.value}("");
+            (bool sent,) = sushiPetsToken.ownerOf(_tokenId).call{value: msg.value}("");
             require(sent, "Ether not sent to the owner of token");
             
             executeBid(_tokenId, msg.sender);   
@@ -61,7 +61,7 @@ contract SushiPetsMarket {
             bid.bidValue = 0;
             bids[_tokenId] = bid;
 
-            emit BidAccepted(_tokenId, ownerOf(_tokenId), msg.sender, bid.bidValue);
+            emit BidAccepted(_tokenId, sushiPetsToken.ownerOf(_tokenId), msg.sender, bid.bidValue);
         } else {
             bid.bidder = msg.sender;
             bid.bidValue = _bidValue;
@@ -76,14 +76,14 @@ contract SushiPetsMarket {
 
         Bid memory bid = bids[_tokenId];
 
-        require(_exists(_tokenId), "ERC721: token has not been minted.");
-        require(msg.sender == ownerOf(_tokenId), "Only the token owner can accept the bid.");
+        require(sushiPetsToken.totalPetsMinted() <= _tokenId, "ERC721: token has not been minted.");
+        require(msg.sender == sushiPetsToken.ownerOf(_tokenId), "Only the token owner can accept the bid.");
         require(bid.bidder != address(0), "Please wait for someone to make the bid before accepting.");
 
         (bool sent,) = msg.sender.call{value: bid.bidValue}("");
         require(sent, "Ether not sent to the owner of token");
 
-        safeTransferFrom(msg.sender, bid.bidder, _tokenId, "");
+        sushiPetsToken.safeTransferFrom(msg.sender, bid.bidder, _tokenId, "");
 
         emit BidAccepted(_tokenId, msg.sender, bid.bidder, bid.bidValue);
 
@@ -92,9 +92,7 @@ contract SushiPetsMarket {
         bids[_tokenId] = bid;
     }
 
-    function executeBid(uint _tokenId, address _bidder) external {
-        require(registeredFactory[msg.sender], "Only a registered NFT factory can execute a bid.");
-
+    function executeBid(uint _tokenId, address _bidder) internal {
         address owner = IERC721(msg.sender).ownerOf(_tokenId);
         IERC721(msg.sender).safeTransferFrom(owner, _bidder, _tokenId, "");
     }
@@ -102,22 +100,22 @@ contract SushiPetsMarket {
     // ========== Getter functions ==========
 
     function getCurrentBidValue(uint _tokenId) public view returns (uint) {
-        require(_exists(_tokenId), "ERC721: token has not been minted.");
+        require(sushiPetsToken.totalPetsMinted() <= _tokenId, "ERC721: token has not been minted.");
         return bids[_tokenId].bidValue;
     }
 
     function getCurrentBidder(uint _tokenId) public view returns (address) {
-        require(_exists(_tokenId), "ERC721: token has not been minted.");
+        require(sushiPetsToken.totalPetsMinted() <= _tokenId, "ERC721: token has not been minted.");
         return bids[_tokenId].bidder;
     }
 
     function getThresholdValue(uint _tokenId) public view returns (uint) {
-        require(_exists(_tokenId), "ERC721: token has not been minted.");
+        require(sushiPetsToken.totalPetsMinted() <= _tokenId, "ERC721: token has not been minted.");
         return threshold[_tokenId].value;
     }
 
     function getThresholdStatus(uint _tokenId) public view returns (bool) {
-        require(_exists(_tokenId), "ERC721: token has not been minted.");
+        require(sushiPetsToken.totalPetsMinted() <= _tokenId, "ERC721: token has not been minted.");
         return threshold[_tokenId].active;
     }
 }
